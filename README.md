ddcc7d15ef6aeea3443680ebc62eabf8b9d5358e
FlandreDaisuki <github@flandre.tw>
2026-07-27 01:04:35 +0800

    feat: init from 2024

---
+++ README.md
@@ -0,0 +1,4 @@
+# resume
+
+- website: <https://resume.flandre.tw>
+- pdf: <https://github.com/FlandreDaisuki/resume/releases/latest/download/Resume_LienChunHao.pdf>
---
+++ scripts/pdf.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { chromium } from 'playwright';
+
+const URL = (process.env.NODE_ENV === 'debug')
+  ? 'http://localhost:3000'
+  : 'https://resume.flandre.tw';
+
+(async() => {
+  const browser = await chromium.launch();
+  const printToPDF = async(url, outName) => {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle' });
+    await page.pdf({
+      path: outName,
+      pageRanges: '1',
+    });
+  };
+
+  await Promise.all([
+    // printToPDF(`${URL}?lang=zh-TW`, './resume.zh-TW.pdf'),
+    // printToPDF(`${URL}?lang=en`, './resume.en.pdf'),
+    printToPDF(URL, './Resume_LienChunHao.pdf'),
+  ]);
+
+  await browser.close();
+})();
